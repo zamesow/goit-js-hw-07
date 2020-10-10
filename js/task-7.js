e7ba634d47f09a3c1ddf9bf8a9103585abbc70f0
Timur Zamesov <zamesow@gmail.com>
2020-10-11 02:03:00 +0300

    Update task-7

--- js/task-7.js
+++ js/task-7.js
@@ -1,7 +1,7 @@
 const inputEl = document.querySelector('#font-size-control');
 const spanEl = document.querySelector('#text');
 
-inputEl.addEventListener('cange', onChangeSizeFont);
+inputEl.addEventListener('input', onChangeSizeFont);
 
 function onChangeSizeFont(event) {
   spanEl.style.fontSize = event.currentTarget.value + 'px';
